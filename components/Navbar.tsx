60b75d28f36539f9b4228df4ee653cfdbb46ed17
Farshid Karimi <farshidk49@gmail.com>
2022-03-23 18:10:55 +0430

    third commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,17 @@ import logo from '../assets/logo.svg'
 import iconCart from '../assets/icon-cart.svg'
 import avatar from '../assets/image-avatar.png'
 import Image from 'next/image'
+import {useState} from 'react'
+import styles from '../styles/Navbar.module.scss'
+import timg1 from '../assets/image-product-1-thumbnail.jpg'
 
-const Navbar = () => {
+
+const Navbar = ({cart, setCart}:any) => {
+
+    const [showCart, setShowCart] = useState(false);
+
   return (
-          <nav className="navbar navbar-expand-lg navbar-light bg-white">
+          <nav className="navbar navbar-expand-lg navbar-light bg-white position-relative">
           <div className="container">
 
             <div>
@@ -14,7 +21,7 @@ const Navbar = () => {
               data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                 <Image src={iconMenu} alt="icon menu" layout='fixed' width="25px" height="25px"/>
             </button>
-            <a className="navbar-brand text-white p-0 m-0" href="#">
+            <a className="navbar-brand text-white p-0 m-0 me-3" href="#">
                 <Image src={logo} alt="icon menu" layout='fixed' width="150px" height="18px"/>
             </a>
             </div>
@@ -24,7 +31,7 @@ const Navbar = () => {
             <div className="offcanvas offcanvas-start-lg bg-white" tabIndex={-1} id="offcanvasExample"  aria-labelledby="offcanvasExampleLabel">
               <div className="offcanvas-header d-flex d-lg-none">
                 <h5 className="offcanvas-title text-white" id="offcanvasExampleLabel">Navbar</h5>
-                <a href="javascript:void(0)"  className="text-reset p-0" data-bs-dismiss="offcanvas"  aria-label="close">
+                <a href="#"  className="text-reset p-0" data-bs-dismiss="offcanvas"  aria-label="close">
                   <svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24" fill="#575767"  viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg>
                 </a>
               </div>
@@ -50,9 +57,26 @@ const Navbar = () => {
             </div>
 
             <div className="mx-3 d-flex align-items-center">
-            <Image src={iconCart} alt="icon cart" layout='fixed' width="50px" height="20px" className="pe-4"/>
+            <Image src={iconCart} alt="icon cart" layout='fixed' width="50px" height="20px" onClick={()=>setShowCart(!showCart)} className="pe-4"/>
             <Image src={avatar} alt="icon cart" layout='fixed' width="40px" height="40px"/>
           </div>
+          </div>
+
+          <div className={`${showCart?styles.cart_preview:styles.cart_preview_hidden} bg-white shadow-sm`} >
+              <p className="p-0 m-0">Cart</p>
+              <hr className="p-0 m-0"/>
+              <div className="buy_list d-flex justify-content-center align-content-center w-100 h-100">
+              {!cart? <span>Your cart is empty</span>:
+                    <div>
+                        <div className="d-flex">
+                          <Image src={timg1} alt="icon menu" layout='fixed'  width="40px" height="40px" className="rounded"/>
+                          <p>Fall Limited Edition Sneakers</p>
+                          <button type="button" className="btn" onClick={()=>setCart(0)} style={{color:"black"}}><svg width="14" height="16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs></svg></button>
+                        </div>
+
+                        <button className="btn text-white fw-bold" type="button" style={{backgroundColor:"hsl(26, 100%, 55%)"}}>Checkout</button>
+                    </div>}
+              </div>
           </div>
         </nav>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,14 @@ const Home: NextPage = () => {
   const [selectImage1, setSelectImage1] = useState(1);
   let [counter, setCounter] = useState(0);
   const [preview, setPreview] = useState(false);
+  let [cart, setCart] = useState(0);
 
   return (
     <div className={styles.sell_page}>
 
     <div className={`${preview?styles.preview_panel_show:styles.preview_panel_hidden} text-center d-flex flex-column justify-content-center align-items-center`}>
-      <div style={{width:"400px", marginRight:"-380px"}}><button className="btn" type="button" onClick={()=>setPreview(false)}><svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fill-rule="evenodd"/></svg></button></div>
-      <Image src={selectImage1===1?img1:selectImage1===2?img2:selectImage1===3?img3:img4}   placeholder="empty" alt="icon menu" layout='fixed' className={`${styles.preview_img} rounded`} width="400px" height="400px"/>
+      <div style={{width:"400px", marginRight:"-380px"}}><button className="btn" type="button" onClick={()=>setPreview(false)}><svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fillRule="evenodd"/></svg></button></div>
+      <Image src={selectImage1===1?img1:selectImage1===2?img2:selectImage1===3?img3:img4}   placeholder="blur" alt="icon menu" layout='fixed' className={`${styles.preview_img} rounded`} width="400px" height="400px"/>
          <div className="btn-group">
            <button type="button" className="btn mx-2" onClick={()=>setSelectImage1(1)}><Image src={timg1} alt="icon menu" layout='fixed'  width="60px" height="60px" className="rounded"/></button>
            <button type="button" className="btn mx-2" onClick={()=>setSelectImage1(2)}><Image src={timg2} alt="icon menu" layout='fixed'  width="60px" height="60px" className="rounded"/></button>
@@ -33,16 +34,16 @@ const Home: NextPage = () => {
          </div>
     </div>
 
-    <Navbar/>
+    <Navbar cart={cart} setCart={setCart}/>
     <div className={`${styles.container} container`}>
           <div className="row mt-3">
               <div className="col-md-6 text-center">
-               <Image onClick={()=>setPreview(true)} src={selectImage===1?img1:selectImage===2?img2:selectImage===3?img3:img4}   placeholder="empty" alt="icon menu" layout='fixed' className={`${styles.preview_img} rounded`} width="400px" height="400px"/>
+               <Image onClick={()=>setPreview(true)} src={selectImage===1?img1:selectImage===2?img2:selectImage===3?img3:img4}   placeholder="blur" alt="icon menu" layout='fixed' className={`${styles.preview_img} rounded`} width="400px" height="400px"/>
                   <div className="btn-group">
-                    <button type="button" className="btn mx-2" onClick={()=>setSelectImage(1)}><Image src={timg1} alt="icon menu" layout='fixed'  width="60px" height="60px" className="rounded"/></button>
-                    <button type="button" className="btn mx-2" onClick={()=>setSelectImage(2)}><Image src={timg2} alt="icon menu" layout='fixed'  width="60px" height="60px" className="rounded"/></button>
-                    <button type="button" className="btn mx-2" onClick={()=>setSelectImage(3)}><Image src={timg3} alt="icon menu" layout='fixed'  width="60px" height="60px" className="rounded"/></button>
-                    <button type="button" className="btn mx-2" onClick={()=>setSelectImage(4)}><Image src={timg4} alt="icon menu" layout='fixed'  width="60px" height="60px" className="rounded"/></button>
+                    <button type="button" className=" mx-3 p-0 pt-2" onClick={()=>setSelectImage(1)}><Image src={timg1} alt="icon menu" layout='fixed'  width="70px" height="70px" className="rounded"/></button>
+                    <button type="button" className=" mx-3 p-0 pt-2" onClick={()=>setSelectImage(2)}><Image src={timg2} alt="icon menu" layout='fixed'  width="70px" height="70px" className="rounded"/></button>
+                    <button type="button" className=" mx-3 p-0 pt-2" onClick={()=>setSelectImage(3)}><Image src={timg3} alt="icon menu" layout='fixed'  width="70px" height="70px" className="rounded"/></button>
+                    <button type="button" className=" mx-3 p-0 pt-2" onClick={()=>setSelectImage(4)}><Image src={timg4} alt="icon menu" layout='fixed'  width="70px" height="70px" className="rounded"/></button>
                   </div>
 
               </div>
@@ -50,20 +51,21 @@ const Home: NextPage = () => {
                     <h5 className="mt-5">Sneaker Company</h5>
                     <h1>Fall Limited Edition Sneakers</h1>
                     <p className="text-muted">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer. </p>
-                    <p className={styles.price}>$125.00</p>
+                    <p className={styles.price}>$125.00  <span style={{backgroundColor:"hsl(25, 100%, 94%)",color:"hsl(26, 100%, 55%)"}}>&nbsp;&nbsp;50%</span></p>
                     <p className="text-decoration-line-through fw-bold text-muted">$250.00</p>
-                    <div className="d-flex mx-2" style={{backgroundColor:"hsl(223, 64%, 98%)"}}>
-                      <div className="w-50 d-flex justify-content-around align-items-center">
-                        <button type="button" className="btn" style={{color:"hsl(26, 100%, 55%)"}} onClick={()=>setCounter(counter<=0?0:counter--)}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
+
+                    <div className="d-flex" >
+                      <div className="w-50 mx-3 d-flex justify-content-around align-items-center" style={{backgroundColor:"hsl(223, 64%, 98%)",boxSizing:"content-box"}}>
+                        <button style={{color:"hsl(26, 100%, 55%)"}} onClick={()=>setCounter(counter<=0?0:counter-1)}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
                           <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
                         </svg></button>
                         <span id="buy_number" className="fw-bold">{counter}</span>
-                        <button type="button" className="btn" style={{color:"hsl(26, 100%, 55%)"}} onClick={()=>setCounter(counter++)}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
+                        <button style={{color:"hsl(26, 100%, 55%)"}} onClick={()=>setCounter(counter+1)}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
                           <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                         </svg></button>
                       </div>
                       <div className="w-50">
-                        <button type="button" className={`btn ${styles.btn_addtobasket} py-2 fw-bold`}><svg width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#ffffff" fillRule="nonzero"/></svg>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   Add to cart</button>
+                        <button type="button" className={`btn ${styles.btn_addtobasket} py-2 fw-bold`} onClick={()=>setCart(counter)}><svg width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#ffffff" fillRule="nonzero"/></svg>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   Add to cart</button>
                       </div>
                     </div>
           </div>
